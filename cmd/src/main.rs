0b2952ec80289099c4901ad33b91224a9acca97f
Itou Ng <13060889537@sina.cn>
2024-04-16 20:37:53 +0800

    feat: An I/O Project: Building a Command Line Program

--- cmd/src/main.rs
+++ cmd/src/main.rs
@@ -1,3 +1,17 @@
+use std::{env, process};
+use cmd::Config;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        eprintln!("Problem parsing arguments: {err}");
+        process::exit(1);
+    });
+
+    if let Err(e) = cmd::run(config) {
+        eprintln!("Application error: {e}");
+        process::exit(1);
+    }
 }
+
